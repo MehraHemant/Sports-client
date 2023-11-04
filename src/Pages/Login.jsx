701c9ab82ec118faf8af249469426ea199843539
MehraHemant <hemantmehra9502@gmail.com>
2023-11-04 12:39:40 +0530

    udpated

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -94,7 +94,6 @@ function Login() {
                                         <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
                                     </div>
                                 </div>
-                                <a href="/" className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">Forgot password?</a>
                             </div>
                             <button type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign in</button>
                             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
